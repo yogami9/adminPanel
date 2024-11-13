262c65690f595e56b1a8a3048f587894f32abbf1
yogami9 <lightwyogami9@gmail.com>
2024-11-13 18:43:58 +0300

    New project created

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // listen on all network interfaces
+    // eslint-disable-next-line no-undef
+    port: Number(process.env.PORT) || 10000, // use the port specified by Render
+  }
 })
